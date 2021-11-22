98137b403a35e3d93c581364bede10409018d375
bibliotekue <80530971+bibliotekue@users.noreply.github.com>
2021-11-22 14:17:04 +0500

    changed project structure because of server issues

--- cv-engine/wsgi.py
+++ cv-engine/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cv project.
+WSGI config for cv-engine project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cv-engine.settings')
 
 application = get_wsgi_application()

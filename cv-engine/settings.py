1ea97eef7f78f2ec53adf0f96013a0d636d31d73
bibliotekue <80530971+bibliotekue@users.noreply.github.com>
2021-11-27 19:12:45 +0500

    summarize staticfiles_dirs and static_root

--- cv-engine/settings.py
+++ cv-engine/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'media')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
